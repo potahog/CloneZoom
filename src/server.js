5edaf6c3ebcd05b5b617e19c9a2462d4785197d5
air <air@moneybrain.ai>
2022-12-09 21:16:30 +0900

    3. Frontend Setup

--- src/server.js
+++ src/server.js
@@ -1,5 +1,16 @@
 import express from "express";
+import path from "path";
+import url from "url";
 
 const app = express();
 
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000);
